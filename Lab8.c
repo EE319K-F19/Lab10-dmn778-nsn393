81a030cbaf8eb91abbf178069ed307cf9153ff77
Dat Nguyen <47252531+dmn778@users.noreply.github.com>
2019-11-27 18:33:26 -0600

    Add files via upload

--- Lab8.c
+++ Lab8.c
@@ -99,11 +99,11 @@ void DrawPlayer(void){
 //check collision method
 //returns 1 if collision between the two sprites is detected
 //0 otherwise
-int CheckCollision(StateType rect1, StateType rect2){
-	if (rect1.x < rect2.x + rect2.width &&
-   rect1.x + rect1.width > rect2.x &&
-   rect1.y < rect2.y + rect2.height &&
-   rect1.y + rect1.height > rect2.y) {
+int CheckCollision(struct State *rect1, struct State *rect2){
+	if (rect1->x < rect2->x + rect2->width &&
+   rect1->x + rect1->width > rect2->x &&
+   rect1->y < rect2->y + rect2->height &&
+   rect1->y + rect1->height > rect2->y) {
     // collision detected!
 		 return 1;
 	}
